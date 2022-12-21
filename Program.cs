e44ec1ab7c7877e4038432fac233e65e571eae39
Ask0nn <Askonzir@gmail.com>
2022-12-21 17:55:54 +0300

    add program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            string[] arr = new string[] { "asd", "12345", "fffda", "++1", "x-", "ffff", "ds" }; //result = [asd, ++1, x-, ds]
+            Console.WriteLine("Исходный массив:\n[ {0} ]", string.Join(", ", arr));
+
+            arr = Array.FindAll(arr, e => e.Length <= 3).ToArray();
+            Console.WriteLine("Измененный массив:\n[ {0} ]", string.Join(", ", arr));
+        }
+    }
+}
